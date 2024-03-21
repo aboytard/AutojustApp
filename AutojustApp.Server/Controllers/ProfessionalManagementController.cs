c88292a7e1dfe8625edaef6b364ae18a2beaa45f
aboytard <alban.boytard@gmail.com>
2024-03-21 16:41:09 +0100

    add one controller for test

---
+++ AutojustApp.Server/Controllers/ProfessionalManagementController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutojustApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfessionalManagementController : ControllerBase 
+    {
+        private readonly ILogger<ProfessionalManagementController> _logger;
+
+        public ProfessionalManagementController(ILogger<ProfessionalManagementController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProfessionalList")]
+        public IEnumerable<Professional> GetProfessionalList()
+        {
+            var professionals = new List<Professional>() 
+            {
+                new Professional()
+                    {
+                        Name = "Alban",
+                        PhoneNumber = "numberOfAlban"
+                    },
+                new Professional()
+                    {
+                        Name = "Kane",
+                        PhoneNumber = "numberOfKane"
+                    },
+            };
+
+            return professionals;
+        }
+
+        [HttpGet(Name = "GetProfessional")]
+        public Professional GetProfessional(string name)
+        {
+            return new Professional()
+            {
+                Name = name,
+                PhoneNumber = "numberOf" + name
+            };
+        }
+
+        [HttpPost(Name = "PostProfessional")]
+        public Professional Post(Professional professional)
+        {
+            return professional;
+        }
+    }
+}
---
+++ AutojustApp.Server/Professional.cs
@@ -0,0 +1,9 @@
+﻿namespace AutojustApp.Server
+{
+    public class Professional
+    {
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
