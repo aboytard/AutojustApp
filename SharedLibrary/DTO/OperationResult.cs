e69f83b4484b2626ac3be4e732d224c1cc982d86
aboytard <alban.boytard@gmail.com>
2024-03-22 18:11:29 +0100

    additional DTO

---
+++ SharedLibrary/DTO/OperationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedLibrary.DTO
+{
+    public class OperationResult
+    {
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public OperationResult() { }
+
+        public OperationResult(string errorMessage)
+        {
+            Success = false;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
---
+++ SharedLibrary/DTO/OperationResultWithData.cs
@@ -0,0 +1,21 @@
+﻿namespace SharedLibrary.DTO
+{
+    public class OperationResultWithData
+    {
+        public dynamic? Data { get; private set; }
+        public bool Success { get; private set; }
+        public string? ErrorMessage { get; set; }
+
+        public OperationResultWithData(dynamic data)
+        {
+            Success = true;
+            Data = data;
+        }
+
+        public OperationResultWithData(bool success, string? errorMessage)
+        {
+            Success = success;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
